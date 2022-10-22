ca118ac0e216bd464f8160feae7bcca782ee508c
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2022-10-22 17:20:58 +0000

    Corrected code

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "egxz2hd", {
+    emailjs.send("service_kckkvte","egxz2hd", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
